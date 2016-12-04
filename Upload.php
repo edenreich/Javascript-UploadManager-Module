dbe7da73f22ef67ca68fce5df5e30bb83947cac4
edenreich <eden.reich@gmail.com>
2016-12-04 22:53:02 +0100

    added some nice features: autoStart, color, callbackAjaxResponse, URL

---
+++ Upload.php
@@ -0,0 +1,7 @@
+<?php
+
+echo 'successfuly uploaded ' . $_FILES['file']['name'];
+
+sleep(1);
+
+?>
--- UploadManager.plugin.js
+++ UploadManager.plugin.js
@@ -39,6 +39,9 @@
 			timeOut: 3000,
 			buttonOnThe: 'right',
 			buttonInnerText: 'Choose',
+			URL: 'Upload.php',
+			autoStart: false,
+			callbackAjaxResponse: function(response) {},
 		}
 
 		settings = defaults;
@@ -61,35 +64,18 @@
 			dropBox = createDropBox();
 
 			container.appendChild(dropBox);
-			dropBox.addEventListener('drop', listFiles);
+			dropBox.ondrop = listFiles;
 		}
 
 		fileInput = createFileInput();
 		chooseButton = createButton(settings);
 		
-		chooseButton.addEventListener('click', openFilesDialogBox);
-		fileInput.addEventListener('change', listFiles);
+		chooseButton.onclick = openFilesDialogBox;
+		fileInput.onchange = listFiles;
 
 		container.appendChild(fileInput);
 		container.appendChild(chooseButton);
 	}
-
-
-	// Extending the default object by letting the object to be overrided if the properties exists.
-	function extendDefaults(source, properties) {
-
-		var property;
-		
-		for(property in properties) {
-
-			if(properties.hasOwnProperty(property)) {
-				source[property] = properties[property];
-			}
-		}
-
-		return source;
-	}
-
 
 	// This method creates the drag and drop box
 	function createDropBox() {
@@ -177,7 +163,7 @@
 		}
 		else if(event.type == 'drop') {
 
-			dropBox.style.border = '1px dotted #a6a6a6';
+			dropBox.style.border = '2px dotted #a6a6a6';
 			dropBoxTitle.style.color = '#a6a6a6';
 			files = event.dataTransfer.files;
 		}
@@ -192,7 +178,12 @@
 		for(var i = 0; i < uploadListItems.length; i++)
 		{
 			container.appendChild(uploadListItems[i]);
+
+			if(settings.autoStart)
+				processFile(files[i], uploadListItems[i]);
 		}
+
+
 		
 		return false;
 	}
@@ -219,7 +210,7 @@
 		list.style.height = '40px';
 		list.style.lineHeight = '35px';
 		list.style.padding = '6px 6px';
-		list.style.border = '1px solid #000000';
+		list.style.border = '1px solid #a6a6a6';
 		list.style.listStyleType = 'none';
 		list.style.marginTop = '5px';
 		list.style.display = 'inline-block';
@@ -232,28 +223,31 @@
 		preview.style.verticalAlign = 'middle';
 		preview.style.marginRight = '10px';
 
-		reader.addEventListener('load', function () {
+		reader.onload = function() {
 				
 			preview.src = this.result;
 
-		}, false);
+		};
 
 		if(file)
 		    reader.readAsDataURL(file);		
 		else
 			preview.src = "";
-	
-		console.log(preview);
 
 		var fileName = document.createElement('span');
 		fileName.innerHTML = file.name;
 
-		list.appendChild(preview);		
-		list.appendChild(fileName);
-		list.appendChild(uploadButton);
-		list.appendChild(removeButton);
-
-		removeButton.addEventListener('click', removeItem);
+		if(!settings.autoStart) {
+			
+			list.appendChild(preview);		
+			list.appendChild(fileName);
+			list.appendChild(uploadButton);
+			list.appendChild(removeButton);
+		
+			removeButton.onclick = removeItem;
+			uploadButton.onclick = processFile;
+			uploadButton.file = file;
+		} 
 
 		return list;
 	}
@@ -273,7 +267,7 @@
 		
 		} else if(event.type == "dragleave") {
 
-			dropBox.style.border = '1px dotted #a6a6a6';
+			dropBox.style.border = '2px dotted #a6a6a6';
 			dropBoxTitle.style.color = '#a6a6a6';
 		}
 	}
@@ -281,10 +275,114 @@
 	// This function processes the files
 	function processFiles(event) {
 
+		var files = event.target.files;
 		
+		console.log(files);
+		// var uploadListItem = this.parentNode;
+		// var progressBar = document.createElement('progress');
+		// var ajax = getXMLHttpRequestObject();
+		// var fd = new FormData();
+
+		// progressBar.value = '0';
+		// progressBar.style.width = '100%';
+		// progressBar.style.height = '100%';
+		// progressBar.max = '100';
 		
-	
-	
+		// fd.append('file', file);
+
+		// empty(uploadListItem);
+
+		// uploadListItem.append(progressBar);
+
+		// ajax.upload.onprogress = updateProgressBar;
+		// ajax.upload.progressBar = progressBar;
+
+		// ajax.open('post', settings.URL);
+		
+		// ajax.onreadystatechange = getAjaxResponse;
+		// ajax.progressBar = progressBar;
+		// ajax.uploadListItem = uploadListItem;
+
+		// ajax.send(fd);
+	}
+
+	// This function processes the files
+	function processFile(event, node) {
+
+		if(event.type == 'click') {
+
+			var uploadListItem = this.parentNode;
+			var file = event.target.file;
+		
+		} else {
+
+			var uploadListItem = node;
+			var file = event;
+		} 
+			
+		
+		var progressBar = document.createElement('progress');
+		var ajax = getXMLHttpRequestObject();
+		var fd = new FormData();
+
+		progressBar.value = '0';
+		progressBar.style.width = '100%';
+		progressBar.style.height = '100%';
+		progressBar.max = '100';
+		
+		fd.append('file', file);
+
+		empty(uploadListItem);
+
+		uploadListItem.append(progressBar);
+
+		ajax.upload.onprogress = updateProgressBar;
+		ajax.upload.progressBar = progressBar;
+
+		ajax.open('post', settings.URL);
+		
+		ajax.onreadystatechange = getAjaxResponse;
+		ajax.progressBar = progressBar;
+		ajax.uploadListItem = uploadListItem;
+
+		ajax.send(fd);
+	}
+
+	// This function update the file upload progressbar
+	function updateProgressBar(event) {
+
+		if(event.lengthComputable) {
+		 	
+			var percentComplete = Math.ceil((event.loaded / event.total) * 100);
+
+			if(percentComplete < 98)
+		    	this.progressBar.value = percentComplete;
+		   
+		  } else {
+		    
+		    throw new Error('Unable to get the progress of the file. Filesize is unknown.')
+		  
+		  }
+
+	}
+
+	// This function handles the response from the server side script
+	function getAjaxResponse() {
+
+		var ajax = this;
+
+		if(ajax.readyState == 4 && ajax.status == 200) {
+		
+			for(var i = 95; i <= 100 ; i++)
+				ajax.progressBar.value = i;
+			
+			setTimeout(function() {
+				ajax.uploadListItem.remove();
+				uploadListItems.pop(ajax.uploadListItem);
+			}, 1000);
+
+			settings.callbackAjaxResponse.call(this, this.response);
+		}
 	}
 
 	// This function opens the files dialog box
@@ -293,10 +391,57 @@
 		document.getElementById('fileInput').click();
 	}
 
+	// This function depends on the browser type returns the XMLHttpRequest object.
+	function getXMLHttpRequestObject() {
+
+	    if (window.XMLHttpRequest) {
+
+	        return new window.XMLHttpRequest;
+
+	    }
+	    else if (window.ActiveXObject) {
+	        
+	        return new ActiveXObject("MSXML2.XMLHTTP.3.0");
+
+	    } else {
+
+	    	throw new Error('This browser does not support ajax');
+
+	    }
+	}
+
+
+	// helper functions
+
+	// remove an item from the upload items
 	function removeItem() {
 
 		this.parentNode.remove();
 		uploadListItems.pop(this);
+	}
+
+	// This function is just for removing all child nodes.
+	function empty(node) {
+		
+		while(node.hasChildNodes()) {
+
+		    node.removeChild(node.lastChild);
+		}
+	}
+
+	// Extending the default object by letting the object to be overrided if the properties exists.
+	function extendDefaults(source, properties) {
+
+		var property;
+		
+		for(property in properties) {
+
+			if(properties.hasOwnProperty(property)) {
+				source[property] = properties[property];
+			}
+		}
+
+		return source;
 	}
 
 
